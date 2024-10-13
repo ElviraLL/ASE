44f84a9ae12483d0689b47144a9bf425bcf9fc5d
ElviraLL <liangjw1012@gmail.com>
2024-10-12 20:29:20 -0700

    WIP carry training

--- inference.sh
+++ inference.sh
@@ -2,4 +2,4 @@ python ase/run.py --test --task HumanoidAMPCarry --num_envs 16 \
 --cfg_env ase/data/cfg/env/humanoid_amp_carry.yaml \
 --cfg_train ase/data/cfg/train/rlg/amp_carry.yaml \
    --motion_file ase/data/cfg/data/pick.yaml \
-   --checkpoint ./output/Humanoid_20241006-00-44-04/nn/Humanoid.pth
+   --checkpoint ./output/Humanoid_20241012-00-44-15/nn/Humanoid.pth
